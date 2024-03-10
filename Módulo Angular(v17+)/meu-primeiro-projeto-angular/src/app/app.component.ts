8df2b1170da45bdf9a8298000016cf8e7992301a
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-10 12:45:30 -0300

    Criando novo componente e adicionando ele no app.component.ts

--- Módulo Angular(v17+)/meu-primeiro-projeto-angular/src/app/app.component.ts
+++ Módulo Angular(v17+)/meu-primeiro-projeto-angular/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NewComponent],
   template: `
     <!-- <router-outlet><router-outlet /> -->
     <h1>Curso Angular</h1>
-    <h2>Components</h2>
+    Antigo: <app-new-component></app-new-component>
+    Novo: <app-new-component />
   `,
 })
 export class AppComponent {}
