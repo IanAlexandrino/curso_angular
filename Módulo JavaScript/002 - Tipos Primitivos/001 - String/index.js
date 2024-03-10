3f327cf965e5d4b74c634d32ec866181021de2db
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-10 00:29:04 -0300

    Reorganizando pastas e adicionando pasta String

---
+++ Módulo JavaScript/002 - Tipos Primitivos/001 - String/index.js
@@ -0,0 +1,13 @@
+console.log("Olá Mundo!")
+console.log('Olá Mundo!')
+console.log(`Olá Mundo!`)
+console.log('"Olá Mundo!"')
+
+console.log(String(1231243))
+
+/*
+    Como podemos escrever uma string
+    'Aqui vai uma string'
+    "Aqui vai uma string"
+    `Aqui vai uma string`
+*/
