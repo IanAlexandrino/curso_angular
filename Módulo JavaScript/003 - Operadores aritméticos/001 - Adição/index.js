db2c570d95e2f3d36393d06a68509bf868752f62
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-10 02:48:18 -0300

    Adicionando pasta de Operadores Aritméticos e pasta de Adição

---
+++ Módulo JavaScript/003 - Operadores aritméticos/001 - Adição/index.js
@@ -0,0 +1,7 @@
+console.log(1 + 1)
+console.log(25 + 50)
+
+console.log("Ian " + "Rodrigues")
+console.log("Nome: " + "Ian Rodrigues" + ", " + "idade: " + 30)
+console.log("Nome: " + "Ian Rodrigues" + ", " + "idade: " + 29 + 1)
+console.log("Nome: " + "Ian Rodrigues" + ", " + "idade: " + (29 + 1))
