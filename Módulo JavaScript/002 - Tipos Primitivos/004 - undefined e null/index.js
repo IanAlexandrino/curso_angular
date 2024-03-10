6c903a00faf4f0ccd2acda351c9458501fc5408e
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-10 00:46:43 -0300

    Adicionando pasta undefined e null

---
+++ Módulo JavaScript/002 - Tipos Primitivos/004 - undefined e null/index.js
@@ -0,0 +1,12 @@
+let resposta
+
+console.log(resposta)
+console.log(undefined)
+console.log(null)
+
+console.log(null === undefined)
+
+/*
+    undefined = algo que não foi definido no momento
+    null = algo que veio nulo
+*/
