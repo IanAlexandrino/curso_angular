4cd801686ed044582a300d577400ebfd10b3c42e
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-10 15:38:44 -0300

    Criando componente template-binding

--- Módulo Angular(v17+)/meu-primeiro-projeto-angular/src/app/app.component.ts
+++ Módulo Angular(v17+)/meu-primeiro-projeto-angular/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+// Components
 import { NewComponent } from './components/new-component/new-component.component';
+import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewComponent],
+  imports: [RouterOutlet, NewComponent, TemplateBindingComponent ],
   styles: [`
     h1 {
       color: red;
@@ -14,10 +17,11 @@ import { NewComponent } from './components/new-component/new-component.component
   template: `
     <!-- <router-outlet><router-outlet /> -->
     <h1>Curso Angular</h1>
-    <div class = "theme-red">
+    <app-template-binding />
+    <!-- <div class = "theme-red">
     Antigo: <app-new-component></app-new-component>
     Novo: <app-new-component />
-    </div>
+    </div> -->
   `,
 })
 export class AppComponent {}
