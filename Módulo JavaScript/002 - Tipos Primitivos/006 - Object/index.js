2224a42dd84071cd7d1317deff8eb167371c347b
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-10 02:38:34 -0300

    Adicionando pasta Object

---
+++ Módulo JavaScript/002 - Tipos Primitivos/006 - Object/index.js
@@ -0,0 +1,78 @@
+/*
+    Objeto: Tenis (Propriedade) "cadarço", "palmilha"
+    Objeto: Celular (Propriedade)
+    Objeto: Drone (Propriedade)
+
+    Propriedades / Atributos / Funcionalidades
+*/
+
+console.log(
+    {
+        tenis: [
+            {
+                preco: 150,
+                disponivel: false,
+                estoque: 150,
+                cadarco: {
+                    cor: "rosa",
+                    tamanho: "G",
+                },
+                palmilha: {
+                    tamanho: 43,
+                    cor: "roxo",
+                }
+            },
+            {
+                preco: 30,
+                disponivel: false,
+                estoque: 5,
+                cadarco: {
+                    cor: "rosa",
+                    tamanho: "G",
+                },
+                palmilha: {
+                    tamanho: 43,
+                    cor: "roxo",
+                }
+            },
+        ]
+    }
+)
+
+console.log([
+    {
+        tenis: [
+            {
+                preco: 150,
+                disponivel: false,
+                estoque: 150,
+                cadarco: {
+                    cor: "rosa",
+                    tamanho: "G",
+                },
+                palmilha: {
+                    tamanho: 43,
+                    cor: "roxo",
+                }
+            },
+            {
+                preco: 30,
+                disponivel: false,
+                estoque: 5,
+                cadarco: {
+                    cor: "rosa",
+                    tamanho: "G",
+                },
+                palmilha: {
+                    tamanho: 43,
+                    cor: "roxo",
+                }
+            },
+        ]
+    },
+    {
+        celular: {
+            marca: "Motorola"
+        }
+    }
+])
