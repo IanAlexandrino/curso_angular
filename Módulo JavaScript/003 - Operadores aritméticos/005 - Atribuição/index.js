a69336743c90a75d87a3a80dc763163be1fcf830
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-10 03:07:47 -0300

    Adicionando pasta Atribuição

---
+++ Módulo JavaScript/003 - Operadores aritméticos/005 - Atribuição/index.js
@@ -0,0 +1,25 @@
+let a = 3
+let b = 2
+
+console.log(a)
+console.log(a + b)
+
+a += 1 //a = a + 1
+
+console.log(a)
+
+a -= 1 //a = a - 1
+
+console.log(a)
+
+a *= 2 //a = a * 2
+
+console.log(a)
+
+a /= 6 //a = a / 6
+
+console.log(a)
+
+/*
+    =, +=, -=, *=, /=
+*/
