1f5729746c5d0fd3c25a26cc1677fe397b770fc0
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-11 03:33:51 -0300

    Testando Event binding

--- Módulo Angular(v17+)/meu-primeiro-projeto-angular/src/app/components/template/template-binding/template-binding.component.ts
+++ Módulo Angular(v17+)/meu-primeiro-projeto-angular/src/app/components/template/template-binding/template-binding.component.ts
@@ -15,7 +15,22 @@ export class TemplateBindingComponent {
   public srcValue = ''
   public isTextDecoration = this.age >= 32 ? 'underline' : 'none'
 
-  public sum (val1: number, val2: number){
-    return val1 + val2;
+  public sum(){
+    return this.age++;
+  }
+
+  public sub(){
+    return this.age--;
+  }
+
+  public onKeyDown(event: Event) {
+    return console.log(event);
+  }
+
+  public onMouseMove(event: MouseEvent){
+    return console.log({
+      clientX: event.clientX,
+      clientY: event.clientY
+    });
   }
 }
